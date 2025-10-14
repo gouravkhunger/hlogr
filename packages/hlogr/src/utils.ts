f6ccfdf1200d784e02382161ad4b4a559e74007f
w <none>
2025-10-14 04:36:53 +0000

    utils: add noColorFormat

--- packages/hlogr/src/utils.ts
+++ packages/hlogr/src/utils.ts
@@ -1,7 +1,12 @@
-import { stylise } from "hlogr/stylise";
+import { stylise, styliseNoColor } from "hlogr/stylise";
 import type { FormatterFn } from "hlogr/types";
 
 export const defaultFormat: FormatterFn = (payload) => {
   const { ip, time, path, status, method, latency, error } = stylise(payload);
+  return [time, status, latency, ip, method, path, error].join(" | ") + "\n";
+};
+
+export const noColorFormat: FormatterFn = (payload) => {
+  const { ip, time, path, status, method, latency, error } = styliseNoColor(payload);
   return [time, status, latency, ip, method, path, error].join(" | ") + "\n";
 };
